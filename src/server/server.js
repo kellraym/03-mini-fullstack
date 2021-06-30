cc5630f3af4e4025cb2a9faa2f424aaa4a33bbf1
akpinkerton <akpinkerton@gmail.com>
2021-06-30 14:03:46 -0700

    DB is functional

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const knex = require('knex')(require('./knexfile.js')['development']);
 const app = express()
 const port = 3001;
 var fs = require('fs');
@@ -10,16 +11,43 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  fs.readFile(locallyStoredInputs, 'utf8', (err, data) => {
-    if (err) {
-      throw(err)
-    } else {
-      const currentList =JSON.parse(data)
-  res.status(201).send(currentList)
-    }
-  })
-})
+
+
+app.get('/', function(req, res) {
+  knex
+    .select('*')
+    .from('user_inputs')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// GETTING DATA USING LOCAL STORAGE
+// app.get('/', (req, res) => {
+//   fs.readFile(locallyStoredInputs, 'utf8', (err, data) => {
+//     if (err) {
+//       throw(err)
+//     } else {
+//       const currentList =JSON.parse(data)
+//   res.status(201).send(currentList)
+//     }
+//   })
+// })
 
 
 app.post('/cheatsheet', (req, res) => {
